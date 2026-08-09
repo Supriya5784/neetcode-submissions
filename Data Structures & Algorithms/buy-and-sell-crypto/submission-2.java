4a06b5fea4ff96b19d6e31e984d33af62f08b996
Supriya Bhattacharjee <supriyab522@gmail.com>
2026-08-09 20:01:35 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int max =0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int price:prices){
+            if(price<minPrice){
+                minPrice = price;
+            }
+            max = Math.max(max,price-minPrice);
+        }
+        return max;
+        
+    }
+}
